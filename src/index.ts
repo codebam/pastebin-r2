75cc62c3e1891cb0d9ea8701069aa364f87a0169
Sean Behan <codebam@riseup.net>
2023-10-19 01:43:16 -0400

    support uploading files

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ app.get('/text/:filename', async (c) => {
 });
 
 const index_page = `
-<html>
+<!DOCTYPE html>
 	<head>
 		<title>Sean's Pastebin</title>
     <meta name="viewport" content="width=device-width">
@@ -63,20 +63,29 @@ const index_page = `
 	<div id="container">
 		<h1>=== Sean's Pastebin ===</h1>
 		<textarea id="paste" rows="20"></textarea>
+		<input type="file" name="file" id="file"/>
 		<button id="submit">submit</button>
 	</div>
 </body>
 <script>
-	window.onload = (event) => {
+	const pastebin = async (container, data) => {
+		const response = await fetch('https://p.seanbehan.ca', {method: 'POST', body: data})
+		const url = await response.text();
+		const url_element = document.createElement('p');
+		url_element.innerHTML = url;
+		container.appendChild(url_element);
+	}
+	window.onload = async (event) => {
 		container = document.getElementById('container');
 		paste = document.getElementById('paste').value;
 		submit = document.getElementById('submit');
+		file = await new Response(document.getElementById('file').files[0]).blob();
 		submit.addEventListener('click', async (event) => {
-			const response = await fetch('https://p.seanbehan.ca', {method: 'POST', body: paste})
-			const url = await response.text();
-			const url_element = document.createElement('p');
-			url_element.innerHTML = url;
-			container.appendChild(url_element);
+			if (paste !== "") {
+				pastebin(container, paste);
+			} else if (file !== "") {
+				pastebin(container, file);
+			}
 		});
 	};
 </script>
