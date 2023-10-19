d85f59954d55817c0868f9fa2585395ccfa2cc20
Sean Behan <codebam@riseup.net>
2023-10-19 17:50:55 -0400

    support moving files

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,16 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.use('*', cors());
 app.use('/info/*', prettyJSON());
+app.post('/:id/:new_id', async (c) => {
+	const id = c.req.param('id');
+	const new_id = c.req.param('new_id');
+	const file = await c.env.R2.get(c.req.param('id'));
+	if (file) {
+		await c.env.R2.put(new_id, await file.blob());
+		await c.env.R2.delete(id);
+	}
+	return c.text('moved\n');
+});
 app.post('/', async (c) => {
 	const id = crypto.randomUUID();
 	await c.env.R2.put(id, await c.req.blob());
@@ -34,7 +44,7 @@ app.get('/:id', async (c) => {
 	return c.text('file not found');
 });
 app.delete('/:id', async (c: any) => {
-	await c.env.R2.delete(c.req.param('filename'));
+	await c.env.R2.delete(c.req.param('id'));
 	return c.text('deleted\n');
 });
 app.get('/text/:id', async (c) => {
