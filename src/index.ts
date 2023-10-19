150406042e38f93f5f9d7ac9b277a2596434ef56
Sean Behan <codebam@riseup.net>
2023-10-19 16:54:07 -0400

    remove newlines

--- src/index.ts
+++ src/index.ts
@@ -8,29 +8,24 @@ const app = new Hono<{ Bindings: Bindings }>();
 
 app.use('*', cors());
 app.use('/info/*', prettyJSON());
-
 app.post('/', async (c) => {
 	const id = crypto.randomUUID();
 	await c.env.R2.put(id, await c.req.blob());
 	return new Response('https://r2.seanbehan.ca/' + id + '\n');
 });
-
 app.get('/info/:id', async (c) => {
 	const file = await c.env.R2.get(c.req.param('id'));
 	return c.json(file);
 });
-
 app.post('/:id', async (c) => {
 	await c.env.R2.put(c.req.param('id'), await c.req.blob());
 	return c.text('https://r2.seanbehan.ca/' + c.req.param('id') + '\n');
 });
-
 app.get('/list', async (c) => {
 	const list = await c.env.R2.list();
 	const files = list.objects.map((obj: { key: string }) => obj.key).join('\n');
 	return c.text(files);
 });
-
 app.get('/:id', async (c) => {
 	const file = await c.env.R2.get(c.req.param('id'));
 	if (file) {
@@ -38,12 +33,10 @@ app.get('/:id', async (c) => {
 	}
 	return c.text('file not found');
 });
-
 app.delete('/:id', async (c: any) => {
 	await c.env.R2.delete(c.req.param('filename'));
 	return c.text('deleted\n');
 });
-
 app.get('/text/:id', async (c) => {
 	const file = await c.env.R2.get(c.req.param('id'));
 	if (file) {
@@ -51,7 +44,6 @@ app.get('/text/:id', async (c) => {
 	}
 	return new Response('file not found');
 });
-
 app.get('/', () => new Response(index_page, { headers: { 'Content-Type': 'text/html' } }));
 
 export default app;
