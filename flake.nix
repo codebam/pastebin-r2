ca0ccc3be59459bf8a116ae51482045c210b6e1a
Sean Behan <codebam@riseup.net>
2024-05-31 23:20:53 -0400

    add nixos packages

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          efm-langserver
+          nil
+          nodePackages_latest.nodejs
+          nodePackages_latest.typescript-language-server
+          nodePackages_latest.prettier
+          vscode-langservers-extracted
+        ];
+      };
+    }
+  );
+}
