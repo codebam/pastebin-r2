c4ae0ce9ec65d3fc5714062052e394ed5c04b3d6
Sean Behan <codebam@riseup.net>
2026-04-28 07:54:56 -0400

    fix flake

--- flake.nix
+++ flake.nix
@@ -2,21 +2,28 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
+  outputs =
     {
-      devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          efm-langserver
-          nil
-          nodePackages_latest.nodejs
-          nodePackages_latest.typescript-language-server
-          nodePackages_latest.prettier
-          vscode-langservers-extracted
-        ];
-      };
-    }
-  );
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            efm-langserver
+            nil
+            nodejs
+            typescript-language-server
+            prettier
+            vscode-langservers-extracted
+          ];
+        };
+      }
+    );
 }
